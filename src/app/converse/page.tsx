827f1a5c769bd78a594186438ba61f0f61caaa12
ehcaw <ryannguyenc@gmail.com>
2024-07-23 01:29:11 +0700

    added basic routing for the main features

---
+++ src/app/converse/page.tsx
@@ -0,0 +1,11 @@
+//src/app/converse/page.tsx
+
+const Converse = () => {
+  return (
+    <div>
+      <h1>this is the conversation page</h1>
+    </div>
+  );
+};
+
+export default Converse;
---
+++ src/app/learn/page.tsx
@@ -0,0 +1,11 @@
+//src/app/learn/page.tsx
+
+const Learn = () => {
+  return (
+    <div>
+      <h1> this is the learning page </h1>
+    </div>
+  );
+};
+
+export default Learn;
